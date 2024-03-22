faf6b0311a94421a27980c90089fb8921cb91c8f
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2024-03-22 07:28:11 +0100

    JS

---
+++ BestShop/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+
+/**
+ * Path to exercise folder
+ */
+const exercisePath = "08_Dzien_4_-_Praca_domowa/02_Flexbox_Grow_Shrink";
+
+/**
+ * Don't change those lines below
+ */
+export default defineConfig({
+  root: exercisePath,
+  server: {
+    port: 3000,
+  },
+});
